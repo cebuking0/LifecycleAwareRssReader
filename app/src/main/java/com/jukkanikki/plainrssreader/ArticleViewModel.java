0523a0cbd7fc34ec873de66db4bbcea42a78fac1
nikkijuk <voitava453auto>
2017-10-30 11:19:34 +0100

    First version of lifecycle aware rss reader - uses view model to simplify feed activity structure and Volley to execute async http get request outside of ui thread

---
+++ app/src/main/java/com/jukkanikki/plainrssreader/ArticleViewModel.java
@@ -0,0 +1,95 @@
+package com.jukkanikki.plainrssreader;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.jukkanikki.plainrssreader.db.AppDatabase;
+import com.jukkanikki.plainrssreader.db.Article;
+import com.jukkanikki.plainrssreader.model.FeedWrapper;
+import com.jukkanikki.plainrssreader.util.ArticlesUtil;
+import com.jukkanikki.plainrssreader.util.DbUtil;
+import com.jukkanikki.plainrssreader.util.PreferencesUtil;
+
+import java.util.List;
+
+public class ArticleViewModel extends AndroidViewModel {
+
+    private static final String TAG = "ArticleViewModel";
+
+    private AppDatabase db;
+
+    private MutableLiveData<List<Article>> articles;
+
+    public ArticleViewModel(@NonNull Application application) {
+        super(application);
+
+        // get db - in memory, so won't live over destroy of app
+        db = AppDatabase.getInMemoryDatabase(application);
+    }
+
+    public LiveData<List<Article>> getArticles() {
+        if (articles == null) {
+            articles = new MutableLiveData<List<Article>>();
+            loadArticles();
+        }
+        return articles;
+    }
+
+    /**
+     *  Do an asyncronous operation to fetch articles.
+     *
+     *  Volley used to fetch data
+     *  - https://developer.android.com/training/volley/index.html
+     */
+    private void loadArticles() {
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this.getApplication());
+
+        // get url from preferences or default
+        String url = PreferencesUtil.getRssUrl(this.getApplication());
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                    // Display the first 500 characters of the response string.
+                    Log.d(TAG,"Response is: "+ response.substring(0,500));
+
+                    // marshal to feed
+                    FeedWrapper feed = ArticlesUtil.convertToObjects(response);
+
+                    // write articles from feed to SQLite db using Room
+                    DbUtil.populateDbFromFeed(db, feed);
+
+                    // get all articles
+                    List<Article> allArticles = db.articleModel().loadAllArticles();
+
+                    // set liveData
+                    articles.setValue(allArticles);
+                }
+            }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // if async operation fails
+                Log.e(TAG, "That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+}
--- app/src/main/java/com/jukkanikki/plainrssreader/FeedActivity.java
+++ app/src/main/java/com/jukkanikki/plainrssreader/FeedActivity.java
@@ -1,28 +1,17 @@
 package com.jukkanikki.plainrssreader;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.jukkanikki.plainrssreader.events.ContentDbReadyReceiver;
-import com.jukkanikki.plainrssreader.events.ContentFileReadyReceiver;
-import com.jukkanikki.plainrssreader.events.Events;
-import com.jukkanikki.plainrssreader.http.HttpReader;
-import com.jukkanikki.plainrssreader.model.FeedWrapper;
-import com.jukkanikki.plainrssreader.services.RssService;
 import com.jukkanikki.plainrssreader.util.ArticlesUtil;
-import com.jukkanikki.plainrssreader.util.PreferencesUtil;
 
 public class FeedActivity extends AppCompatActivity {
 
@@ -30,14 +19,6 @@ public class FeedActivity extends AppCompatActivity {
 
     // view to show feeds
     private RecyclerView articleView;
-
-    // receiver for content ready notifications
-
-    // --> content from dbn
-    //ContentFileReadyReceiver contentReadyReceiver = new ContentFileReadyReceiver();
-
-    // --> content from db
-    ContentDbReadyReceiver contentReadyReceiver = new ContentDbReadyReceiver();
 
     /**
     * Called when activity is created
@@ -57,36 +38,19 @@ public class FeedActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager  = new LinearLayoutManager(getBaseContext(),LinearLayoutManager.VERTICAL,false);
         articleView.setLayoutManager(linearLayoutManager);
 
-        // define which view is updated when content is ready
-        contentReadyReceiver.setArticleView(articleView);
+        // Create a ViewModel the first time the system calls an activity's onCreate() method.
+        // Re-created activities receive the same MyViewModel instance created by the first activity.
+
+        ArticleViewModel model = ViewModelProviders.of(this).get(ArticleViewModel.class);
+
+        // start observing state of view model
+        // important: observer binds articles to acticle view
+        // important: lambda syntax needs jdk 1.8 as target and works without jack with android studio 3.0
+        model.getArticles().observe(this, articles ->
+            ArticlesUtil.bindViewToArticles(this ,articleView, articles)
+        );
+
     }
-
-    /**
-    * Called when activity comes visible
-    * As feed is loaded at onResume list of articles is refreshed always when activity comes visible
-    */
-    @Override
-    protected void onResume () {
-        super.onResume();
-        registerContentReadyReceiver(); // listen content ready events
-
-
-        // call backgroud service to read feed
-
-        //loadRssUsingAsyncTask(); // NOTE: async task
-
-        loadRssUsingIntentService(); // NOTE: intent service + broadcast receiver
-    }
-
-    /**
-     * Called when activity stops
-     */
-    @Override
-    protected void onPause () {
-        super.onPause();
-        unRegisterContentReadyReceiver(); // stop listening content ready events
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -116,96 +80,6 @@ public class FeedActivity extends AppCompatActivity {
     public void openSettings(View view) {
         Intent settingsIntent = new Intent(this,SettingsActivity.class);
         startActivity(settingsIntent); // opens settings activity
-    }
-
-    /**
-     * Load rss feed content asynchronously
-     *
-     * this method is kinda ok, but as activity might just die without saying goodbye
-     * it might be that when async task is finished there's nothing that could be updated
-     * (original activity of which async task was part is gone)
-     */
-    private void loadRssUsingAsyncTask() {
-        AsyncTask<String, String, String> loadRSSAsync = new AsyncTask<String, String, String>() {
-
-            @Override
-            protected void onPreExecute() {
-                // no initialization needed
-            }
-
-            @Override
-            protected String doInBackground(String... params) {
-                return HttpReader.getData(params[0]); // get http string
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-
-                // at that point activity might be already away if
-                // android has needed memory or configuration changes have happened
-
-                fillArticlesView(s); // set result to acticles view
-            }
-        };
-
-        String rssUrl = PreferencesUtil.getRssUrl(this); // get url from preferences or default
-
-        // start execution of async task
-        // at that point activity exists always
-        loadRSSAsync.execute(rssUrl);
-    }
-
-    /**
-     * Fill list of articles
-     * @param json json content
-     */
-    private void fillArticlesView(String json) {
-        FeedWrapper feed = ArticlesUtil.convertToObjects(json);     // currently displayed feed
-
-        // if activity has died also article view is away and base context can't be found either
-        ArticlesUtil.bindViewToFeed(getBaseContext(),articleView, feed);
-    }
-
-    /**
-     * Register local receiver for content ready broadcasts
-     */
-    private void registerContentReadyReceiver() {
-
-        // The filter's action is BROADCAST_ACTION
-        IntentFilter contentReadyIntentFilter = new IntentFilter(Events.CONTENT_READY_ACTION);
-
-        // Registers the receiver and its intent filters
-        LocalBroadcastManager.getInstance(this).registerReceiver(
-                contentReadyReceiver,
-                contentReadyIntentFilter);
-
-        Log.d(TAG,"Registered content ready receiver");
-    }
-
-    /**
-     * UnRegister local receiver for content ready broadcasts
-     */
-    private void unRegisterContentReadyReceiver() {
-        // UnRegisters the receiver
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(contentReadyReceiver);
-
-        Log.d(TAG,"Unregistered content ready receiver");
-    }
-
-
-    /**
-     * Creates a new Intent to start the RssService. Passes a URI in the Intent's "data" field.
-     */
-    private void loadRssUsingIntentService() {
-        String rssUrl = PreferencesUtil.getRssUrl(this); // get url from preferences or default
-        Intent intent = new Intent(this, RssService.class); // define which service is to be started
-        intent.setData(Uri.parse(rssUrl)); // set url to data
-
-        // Starts the IntentService
-        // intent service is started service and doesn't have any kind of reference to activity calling it
-        startService(intent);
-
-        Log.d(TAG,"Called rss service");
     }
 
 }
